ac3fabc007e858f910b80af239add2156a6c2705
liujl <oiygbh@163.com>
2021-12-27 22:33:24 +0800

    封装、继承和多态

---
+++ class_07day/class1.py
@@ -0,0 +1,38 @@
+# @Time : 2021/12/27 21:43 
+# @Author :jiale
+# @File : class1.py 
+# @Software: PyCharm
+# 自定义属性访问
+class Test():
+    def __init__(self):
+        self.age=18
+    # 当我们访问属性的时候，如果属性不存在(出现AttrError)，该方法会被触发
+    def __getattr__(self, item):
+        print("-----这个是__getattr__")
+        return 100
+    # 查找属性的时候，第一时间触发该方法去找属性
+    def __getattribute__(self, item):
+        print("-----这个是__getattribute__方法")
+        # return 999
+        return super().__getattribute__(item) # 会触发__getattr__方法
+    def __setattr__(self, key, value):
+        if key =='age':
+            super().__setattr__(key,18)
+
+        else:
+        # 这个方法在给对象设置属性时触发
+            print("-----设置属性的时候会触发这个方法-----")
+            print(key)
+            print(value)
+            super().__setattr__(key,value)
+    def __delattr__(self, item):
+        print(item)
+        # 这个方法在删除属性的时候会被触发
+        print("-----__delattr__被调用-----")
+        super().__delattr__(item)
+t=Test()
+t.time=10
+t.age=999
+del t.time  # 删除属性
+print(t.time)
+print(t.age)
---
+++ class_07day/class2.py
@@ -0,0 +1,33 @@
+# @Time : 2021/12/27 22:16 
+# @Author :jiale
+# @File : class2.py 
+# @Software: PyCharm
+class Filed(object):
+    # 一个类中只要出现以下三个方法的任意一个，那么该类就是描述器类
+
+    def __get__(self, instance, owner):
+        print("触发了__get__方法")
+
+    def __set__(self, instance, value):
+        print("触发了__set__方法")
+        self.value = value
+        print(self)
+        print(instance)
+        print(value)
+    def __delete__(self, instance):
+        print("触发了__delete__方法")
+        print(self)
+        self.value=None
+
+
+
+class Model(object):
+    name = 'toby'
+    attr = Filed()  # 储存的是描述器对象，会覆盖类属性
+
+
+m = Model()
+m.name = '000'
+m.attr = 100
+print(m.name)
+print(m.attr)
